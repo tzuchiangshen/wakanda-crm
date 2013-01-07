02f28fb5367b974cc0dafbaa12f1ac1f2c898f93
David Robbins <dave@wakanda.org>
2013-01-07 11:48:50 -0800

    accounts now available again for quick add.

--- CRM/WebFolder/sideBar.waComponent/sideBar.js
+++ CRM/WebFolder/sideBar.waComponent/sideBar.js
@@ -4,9 +4,18 @@
 // Add the code that needs to be shared between components here
 
 function constructor (id) {
-	var quickAddLeadsContainer = getHtmlId('quickAddLeadsContainer'),
+	var quickAddMainContainer = getHtmlId('quickAddMainContainer'),
+		//Leads
+		quickAddBodyContainerLeads = getHtmlId('quickAddBodyContainerLeads'),
 		quickAddFirstNameLeads = getHtmlId('quickAddFirstNameLeads'),
 		quickAddLastNameLeads = getHtmlId('quickAddLastNameLeads'),
+		//Accounts
+		quickAddBodyContainerAccounts = getHtmlId('quickAddBodyContainerAccounts'),
+		quickAddAccountName = getHtmlId('quickAddAccountName'),
+		quickAddAccountPhone = getHtmlId('quickAddAccountPhone'),
+		quickAddAccountWebsite = getHtmlId('quickAddAccountWebsite'),
+		//Datasources
+		accountDataSource = waf.sources.account,
 		leadDataSource = waf.sources.lead;
 	
 	// @region beginComponentDeclaration// @startlock
@@ -16,16 +25,68 @@ function constructor (id) {
 
 	this.load = function (data) {// @lock
 		//console.log(data.userData.menuItem);
+		switch(data.userData.menuItem)
+		{
+			case "leads":
+			$$(quickAddMainContainer).show();
+			break;
+			
+			case "accounts":
+			//$$(quickAddMainContainer).hide();
+			$("#" + quickAddBodyContainerAccounts).css("top", "30px");
+			$("#" + quickAddBodyContainerAccounts).css("left", "0px");
+			$$(quickAddBodyContainerLeads).hide();
+			$$(quickAddBodyContainerAccounts).show();
+			break;
+			
+			case "contacts":
+			$$(quickAddMainContainer).hide();
+			break;
+			
+			default:
+			$$(quickAddMainContainer).hide();
+		}
+		
+		/*
 		if (data.userData.menuItem == "leads") {
 			$$(quickAddLeadsContainer).show();
 		} else {
 			$$(quickAddLeadsContainer).hide();
 		}
+		*/
 	// @region namespaceDeclaration// @startlock
+	var quickAddAccountsSaveButton = {};	// @button
 	var quickAddLeadsSaveButton = {};	// @button
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
+
+	quickAddAccountsSaveButton.click = function quickAddAccountsSaveButton_click (event)// @startlock
+	{// @endlock
+		accountDataSource.addNewElement();
+		accountDataSource.serverRefresh({
+			onSuccess:function(event){
+		        // the new entity has been initialized by the server
+		        //Now update the new entity with the form values.
+		        accountDataSource.name = $$(quickAddAccountName).getValue();
+				accountDataSource.phone = $$(quickAddPhone).getValue();
+				accountDataSource.website = $$(quickAddWebsite).getValue();
+				//accountDataSource.billingCity = $$(quickAddCity).getValue();
+				//accountDataSource.billingCountry = $$(quickAddCountry).getValue();
+				//Save the entity.
+				accountDataSource.save();
+				accountDataSource.autoDispatch();
+				//reset form
+				$$(quickAddAccountName).setValue();
+				$$(quickAddPhone).setValue();
+				$$(quickAddWebsite).setValue();
+				//$$(quickAddCity).setValue();
+				//$$(quickAddCountry).setValue();
+				
+				$('#' + quickAddAccountName).focus();
+			}
+   		});
+	};// @lock
 
 	quickAddLeadsSaveButton.click = function quickAddLeadsSaveButton_click (event)// @startlock
 	{// @endlock
@@ -51,6 +112,7 @@ function constructor (id) {
 	};// @lock
 
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_quickAddAccountsSaveButton", "click", quickAddAccountsSaveButton.click, "WAF");
 	WAF.addListener(this.id + "_quickAddLeadsSaveButton", "click", quickAddLeadsSaveButton.click, "WAF");
 	// @endregion// @endlock
 
