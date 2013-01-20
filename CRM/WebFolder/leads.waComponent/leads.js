efbc1e718709fc38e2e056f643d7f1008bdb78e7
David Robbins <dave@wakanda.org>
2013-01-20 10:37:08 -0800

    added convert league dialog.

--- CRM/WebFolder/leads.waComponent/leads.js
+++ CRM/WebFolder/leads.waComponent/leads.js
@@ -4,7 +4,8 @@
 // Add the code that needs to be shared between components here
 
 function constructor (id) {
-	var leadsReportsComponent = getHtmlId('leadsReportsComponent');
+	var leadsReportsComponent = getHtmlId('leadsReportsComponent'),
+		convertLeadTitleDetail = getHtmlId('convertLeadTitleDetail');
 	
 	// @region beginComponentDeclaration// @startlock
 	var $comp = this;
@@ -21,6 +22,8 @@ function constructor (id) {
 		$$(leadsReportsComponent).loadComponent({path: '/reports.waComponent', userData: {menuItem: "leads"}});
 			
 	// @region namespaceDeclaration// @startlock
+	var button3 = {};	// @button
+	var button2 = {};	// @button
 	var convertButton = {};	// @button
 	var button1 = {};	// @button
 	var leadsCancelButton = {};	// @button
@@ -29,15 +32,34 @@ function constructor (id) {
 
 	// eventHandlers// @lock
 
-	convertButton.click = function convertButton_click (event)// @startlock
+	button3.click = function button3_click (event)// @startlock
 	{// @endlock
-		//Convert The Lead.
+		//Convert Lead Button
 		waf.sources.lead.converted = true;
 		waf.sources.lead.save({
 			onSuccess: function(event) {
-				
+				waf.sources.lead.query("converted == false", {
+					onSuccess: function(event) {
+						$$(id + "_tabView2").selectTab(1);
+					} //onSuccess
+				});
 			} //onSuccess
 		}); //waf.sources.lead.save
+		//end - Convert The Lead.
+	};// @lock
+
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+		//Convert Cancel Button
+		$$(id + "_tabView2").selectTab(2);
+	};// @lock
+
+	convertButton.click = function convertButton_click (event)// @startlock
+	{// @endlock
+		//Convert Lead - Show Dialog Button
+		$('#' + id + "_convertLeadTitleDetail").html("( " + waf.sources.lead.firstName + " " + waf.sources.lead.lastName + " : " + waf.sources.lead.company + " )");
+		//convertLeadTitleDetail.setValue("(" + waf.sources.lead.firstName + " " + waf.sources.lead.lastName + ")");
+		$$(id + "_tabView2").selectTab(3);
 	};// @lock
 
 	button1.click = function button1_click (event)// @startlock
@@ -60,6 +82,8 @@ function constructor (id) {
 	};// @lock
 
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_button3", "click", button3.click, "WAF");
+	WAF.addListener(this.id + "_button2", "click", button2.click, "WAF");
 	WAF.addListener(this.id + "_convertButton", "click", convertButton.click, "WAF");
 	WAF.addListener(this.id + "_button1", "click", button1.click, "WAF");
 	WAF.addListener(this.id + "_leadsCancelButton", "click", leadsCancelButton.click, "WAF");
