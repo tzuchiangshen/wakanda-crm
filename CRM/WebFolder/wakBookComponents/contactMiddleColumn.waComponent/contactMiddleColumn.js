4d94a8b946eedbfc540faee8a22508d6ba79fefc
David Robbins <dave@wakanda.org>
2013-05-20 11:25:48 -0700

    wakBook make new contacts component - contactsList.

--- CRM/WebFolder/wakBookComponents/contactMiddleColumn.waComponent/contactMiddleColumn.js
+++ CRM/WebFolder/wakBookComponents/contactMiddleColumn.waComponent/contactMiddleColumn.js
@@ -4,14 +4,23 @@
 // Add the code that needs to be shared between components here
 
 function constructor (id) {
-
+	var middleColumnContainer = getHtmlId("middleColumnContainer"),
+		middleColumnContainer$ = getHtmlObj("middleColumnContainer"),
+		contactsList = getHtmlId("contactsList"),
+		contactsList$ = getHtmlObj("contactsList");
+		
 	// @region beginComponentDeclaration// @startlock
 	var $comp = this;
 	this.name = 'contactMiddleColumn';
 	// @endregion// @endlock
 
+	function loadContacts() {
+	
+	}
+	
 	this.load = function (data) {// @lock
-
+		console.log(contactsList$);
+		
 	// @region namespaceDeclaration// @startlock
 	// @endregion// @endlock
 
