c174a92186eac4b1d551089b4aeaad411c48eae4
David Robbins <dave@wakanda.org>
2013-05-20 12:39:33 -0700

    added buildContactsGrid() function.

--- CRM/WebFolder/wakBookComponents/contactsList.waComponent/contactsList.js
+++ CRM/WebFolder/wakBookComponents/contactsList.waComponent/contactsList.js
@@ -4,14 +4,38 @@
 // Add the code that needs to be shared between components here
 
 function constructor (id) {
-
+	var contactsUL = getHtmlId('contactsUL'),
+		contactsUL$ = getHtmlObj('contactsUL');
+	
+		
 	// @region beginComponentDeclaration// @startlock
 	var $comp = this;
 	this.name = 'contactsList';
 	// @endregion// @endlock
-
+	function buildContactGrid() {
+		contactsUL$.children().remove();
+		
+		/**/
+		ds.Contact.all({
+			onSuccess: function(ev1) {
+				console.log(ev1.entityCollection.length);
+				ev1.entityCollection.forEach({
+					onSuccess: function(ev2) {	
+						var contactLi = $('<li>', {
+							text: ev2.entity.lastName.getValue(),
+							"class" : "contactPreview"
+						});
+						
+						contactsUL$.append(contactLi);
+					}
+				}); //ev1.entityCollection.forEach
+			}
+		});
+		
+	}
+	
 	this.load = function (data) {// @lock
-
+		buildContactGrid();
 	// @region namespaceDeclaration// @startlock
 	// @endregion// @endlock
 
